bd71bcb3fb85519e1605fc4a01f731eab7ddf2d9
pmendozans <pmendoza@nearsoft.com>
2018-02-13 17:18:46 -0700

    Fixed identation

--- mapsPlaces/New Group3/GoogleMaps/GooglePlace.swift
+++ mapsPlaces/New Group3/GoogleMaps/GooglePlace.swift
@@ -36,36 +36,36 @@ import CoreLocation
 import SwiftyJSON
 
 class GooglePlace {
-  let name: String
-  let address: String
-  let coordinate: CLLocationCoordinate2D
-  let placeType: String
-  var photoReference: String?
-  var photo: UIImage?
-  
-  init(dictionary: [String: Any], acceptedTypes: [String])
-  {
-    let json = JSON(dictionary)
-    name = json["name"].stringValue
-    address = json["vicinity"].stringValue
+    let name: String
+    let address: String
+    let coordinate: CLLocationCoordinate2D
+    let placeType: String
+    var photoReference: String?
+    var photo: UIImage?
     
-    let lat = json["geometry"]["location"]["lat"].doubleValue as CLLocationDegrees
-    let lng = json["geometry"]["location"]["lng"].doubleValue as CLLocationDegrees
-    coordinate = CLLocationCoordinate2DMake(lat, lng)
-    
-    photoReference = json["photos"][0]["photo_reference"].string
-    
-    var foundType = "restaurant"
-    let possibleTypes = acceptedTypes.count > 0 ? acceptedTypes : ["bakery", "bar", "cafe", "grocery_or_supermarket", "restaurant"]
-    
-    if let types = json["types"].arrayObject as? [String] {
-      for type in types {
-        if possibleTypes.contains(type) {
-          foundType = type
-          break
+    init(dictionary: [String: Any], acceptedTypes: [String])
+    {
+        let json = JSON(dictionary)
+        name = json["name"].stringValue
+        address = json["vicinity"].stringValue
+        
+        let lat = json["geometry"]["location"]["lat"].doubleValue as CLLocationDegrees
+        let lng = json["geometry"]["location"]["lng"].doubleValue as CLLocationDegrees
+        coordinate = CLLocationCoordinate2DMake(lat, lng)
+        
+        photoReference = json["photos"][0]["photo_reference"].string
+        
+        var foundType = "restaurant"
+        let possibleTypes = acceptedTypes.count > 0 ? acceptedTypes : ["bakery", "bar", "cafe", "grocery_or_supermarket", "restaurant"]
+        
+        if let types = json["types"].arrayObject as? [String] {
+            for type in types {
+                if possibleTypes.contains(type) {
+                    foundType = type
+                    break
+                }
+            }
         }
-      }
+        placeType = foundType
     }
-      placeType = foundType
-  }
 }
--- mapsPlaces/New Group3/GoogleMaps/PlaceMarker.swift
+++ mapsPlaces/New Group3/GoogleMaps/PlaceMarker.swift
@@ -34,15 +34,15 @@ import UIKit
 import GoogleMaps
 
 class PlaceMarker: GMSMarker {
-  let place: GooglePlace
-  
-  init(place: GooglePlace) {
-    self.place = place
-    super.init()
+    let place: GooglePlace
     
-    position = place.coordinate
-    icon = UIImage(named: place.placeType+"_pin")
-    groundAnchor = CGPoint(x: 0.5, y: 1)
-    appearAnimation = .pop
-  }
+    init(place: GooglePlace) {
+        self.place = place
+        super.init()
+        
+        position = place.coordinate
+        icon = UIImage(named: place.placeType+"_pin")
+        groundAnchor = CGPoint(x: 0.5, y: 1)
+        appearAnimation = .pop
+    }
 }
